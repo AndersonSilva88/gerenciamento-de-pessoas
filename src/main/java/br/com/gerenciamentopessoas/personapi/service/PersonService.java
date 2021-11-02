60c45b0466c264ad098fb8edf6ad76a411db7757
perkles <anderson.os88@hotmail.com>
2021-11-02 11:22:00 -0300

    Developed update by id feature

--- src/main/java/br/com/gerenciamentopessoas/personapi/service/PersonService.java
+++ src/main/java/br/com/gerenciamentopessoas/personapi/service/PersonService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -59,5 +58,17 @@ public class PersonService {
     private Person verifyIfExists(Long id) throws PersonNotFoundException{
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToUpdate);
+        return MessageResponseDTO
+                .builder()
+                .message("Updated person with ID " + savedPerson.getId())
+                .build();
     }
 }
